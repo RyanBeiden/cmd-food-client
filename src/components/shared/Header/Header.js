1f58b2b226f6e8393d0651d3135809e6aa77c111
Ryan Beiden <rdbeiden@gmail.com>
2021-03-19 16:16:59 -0500

    wip / add header with text and profile image still needs ternaries for profile image and dropdown functionality for menu

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import './Header.scss';
+
 function Header(props) {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 500">
+        <path
+          fill="#116466"
+          fillOpacity="1"
+          d="M0,500L1440,300L1440,0L0,0Z">
+        </path>
+      </svg>
+      <div className="headerText">
+        <h1>Cmd + Food</h1>
+        <h3>Plan your next grocery trip.</h3>
+      </div>
+      <img
+        className='profile-image'
+        alt='Profile'
+        src='https://res.cloudinary.com/hhrrinvop/image/upload/v1616017542/cmd-food-assets/media/profiles/LINKED-IN-PROFILE-1_yzxjn9.jpg'
+      />
+    </header>
   );
 }
 
