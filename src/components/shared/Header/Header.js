d8bfaeb54066e15b8f9a0c332529ec1f26c5e110
Ryan Beiden <rdbeiden@gmail.com>
2021-03-19 17:18:47 -0500

    wip / remove mateiral ui, add bootstrap & create image dropdown on click Still needs ternary for when the dropdown is displayed as well as user data pulled in from `/profile`

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import './Header.scss';
 
@@ -12,15 +14,23 @@ function Header(props) {
           d="M0,500L1440,300L1440,0L0,0Z">
         </path>
       </svg>
-      <div className="headerText">
+      <div className="header-text">
         <h1>Cmd + Food</h1>
         <h3>Plan your next grocery trip.</h3>
       </div>
-      <img
-        className='profile-image'
-        alt='Profile'
-        src='https://res.cloudinary.com/hhrrinvop/image/upload/v1616017542/cmd-food-assets/media/profiles/LINKED-IN-PROFILE-1_yzxjn9.jpg'
-      />
+      <div className='dropdown-container'>
+        <DropdownButton variant="secondary" id="dropdown-button" title={
+          <img
+            className="thumbnail-image"
+            src='https://res.cloudinary.com/hhrrinvop/image/upload/v1616017542/cmd-food-assets/media/profiles/LINKED-IN-PROFILE-1_yzxjn9.jpg'
+            alt="Profile"
+          />
+        }>
+          <Dropdown.Item href="/">Profile</Dropdown.Item>
+          <Dropdown.Item href="/">Basket</Dropdown.Item>
+          <Dropdown.Item href="/">Start Shopping</Dropdown.Item>
+        </DropdownButton>
+      </div>
     </header>
   );
 }
