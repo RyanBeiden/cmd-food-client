8278cf497316088acee3a15e9b22fae96c9ed0de
Ryan Beiden <rdbeiden@gmail.com>
2021-03-20 17:29:24 -0500

    show default profile image

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,27 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
 import './Header.scss';
 
 function Header(props) {
-  const [profile, setProfile] = useState({});
-
-  const getProfile = async () => {
-    const response = await fetch('https://cmd-food.herokuapp.com/profiles', {
-      method: 'GET',
-      headers: {
-        Authorization: `Token ${localStorage.getItem('cf_token')}`,
-      },
-    });
-    const value = await response.json();
-    return setProfile(value);
-  };
-
-  useEffect(() => {
-    getProfile();
-  }, []);
-
   return (
     <header>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 500">
@@ -38,17 +21,12 @@ function Header(props) {
       {localStorage.getItem('cf_token') !== null
         ? <div className='dropdown-container'>
           <DropdownButton variant="secondary" id="dropdown-button"
-            title={profile[0] && profile[0].profile_img !== ''
-              ? <img
-                  className="thumbnail-image"
-                  src={profile[0].profile_img}
-                  alt="Profile"
-                />
-              : <img
-                  className="thumbnail-image"
-                  src='https://res.cloudinary.com/hhrrinvop/image/upload/v1616271222/cmd-food-assets/media/profiles/placeholder-profile-image_ey6lff.jpg'
-                  alt="Profile"
-                />
+            title={
+              <img
+                className="thumbnail-image"
+                src='https://res.cloudinary.com/hhrrinvop/image/upload/v1616271222/cmd-food-assets/media/profiles/placeholder-profile-image_ey6lff.jpg'
+                alt="Profile"
+              />
             }>
             <Link to='/profile' className='nav-link'>Profile</Link>
             <Link to='/Basket' className='nav-link'>Basket</Link>
