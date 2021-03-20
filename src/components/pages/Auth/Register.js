3a1c9fa923e7a512eddf62d60bf00535807b9d95
Ryan Beiden <rdbeiden@gmail.com>
2021-03-20 14:14:06 -0500

    shopper registration complete

--- src/components/pages/Auth/Register.js
+++ src/components/pages/Auth/Register.js
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import { AuthContext } from '../../../helpers/data/AuthProvider';
 
 function Register(props) {
+  const { createShopper } = useContext(AuthContext);
+
+  const [show, setShow] = useState(false);
+  const [shopper, setShopper] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleInputChange = (e) => {
+    const newShopperState = { ...shopper };
+    newShopperState[e.target.id] = e.target.value;
+    setShopper(newShopperState);
+  };
+
+  const saveShopper = async (e) => {
+    e.preventDefault();
+    if (shopper.password === shopper.confirmPassword) {
+      const newShopper = {
+        first_name: shopper.firstName,
+        last_name: shopper.lastName,
+        email: shopper.email,
+        password: shopper.password,
+        profile_img: '',
+      };
+
+      await createShopper(newShopper)
+        .then((response) => {
+          if ('token' in response) {
+            localStorage.setItem('cf_token', response.token);
+          }
+        })
+        .then(() => props.history.push('/profile'));
+    } else {
+      setShow(true);
+    }
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className='register'>
+      <Form>
+        <Form.Group controlId="firstName">
+          <Form.Label>First Name *</Form.Label>
+          <Form.Control type="text" onChange={handleInputChange} required />
+        </Form.Group>
+        <Form.Group controlId="lastName">
+          <Form.Label>Last Name *</Form.Label>
+          <Form.Control type="text" onChange={handleInputChange} required />
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email *</Form.Label>
+          <Form.Control type="email" onChange={handleInputChange} required />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password *</Form.Label>
+          <Form.Control type="password" onChange={handleInputChange} required />
+        </Form.Group>
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Confirm Password *</Form.Label>
+          <Form.Control type="password" onChange={handleInputChange} required />
+        </Form.Group>
+        {show
+          ? <Alert className='password-alert' variant="danger" onClose={() => setShow(false)} dismissible>
+              <p>Passwords do not match.</p>
+            </Alert>
+          : ''
+        }
+        <div className="formSubmit">
+          <Button type="submit" onClick={saveShopper}>Register</Button>
+        </div>
+      </Form>
     </div>
   );
 }
--- src/helpers/data/ProfileProvider.js
+++ src/helpers/data/ProfileProvider.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const ProfileContext = React.createContext();
 
 function ProfileProvider(props) {
+  const [profile, setProfile] = useState([]);
+
+  const getProfile = async () => {
+    const response = await fetch('https://cmd-food.herokuapp.com/profile', {
+      method: 'GET',
+      headers: {
+        Authorization: `Token ${localStorage.getItem('cf_token')}`,
+      },
+    });
+    const value = await response.json();
+    return setProfile(value);
+  };
+
   return (
-    <ProfileContext.Provider value={ 'ProfileProvider' }>
+    <ProfileContext.Provider value={{ getProfile, profile }}>
       { props.children }
     </ProfileContext.Provider>
   );
