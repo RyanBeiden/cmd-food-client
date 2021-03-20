16e45f6582023dff86e69e8df44e8e568040d2a1
Ryan Beiden <rdbeiden@gmail.com>
2021-03-20 18:22:43 -0500

    add nav buttons with working links for authed user home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import PersonIcon from '@material-ui/icons/Person';
+import './Home.scss';
 
 function Home(props) {
+  const [profileId, setProfileId] = useState(0);
+
+  const getProfile = () => new Promise((resolve, reject) => {
+    fetch('https://cmd-food.herokuapp.com/profiles', {
+      method: 'GET',
+      headers: {
+        Authorization: `Token ${localStorage.getItem('cf_token')}`,
+      },
+    })
+      .then((res) => resolve(res.json()))
+      .catch((err) => reject(err));
+  });
+
+  useEffect(() => {
+    getProfile()
+      .then((res) => setProfileId(res[0].id));
+  }, []);
+
   return (
-    <div>
-      {
-        props.history.location.pathname === '/authentication'
-          ? <h1>Authentication Home</h1>
-          : <h1>Authorized Home</h1>
-      }
+    <div className='home'>
+      {profileId !== 0
+        ? <>
+            <div className='button-container'>
+              <Link to={`/basket/${profileId}`}>
+                <button className='nav-link'><ShoppingBasketIcon className='nav-basket' />Basket</button>
+              </Link>
+            </div>
+            <div className='button-container'>
+              <Link to='/start'>
+                <button className='nav-link'><PlayArrowIcon className='nav-start' />Start Shopping</button>
+              </Link>
+            </div>
+            <div className='button-container'>
+              <Link to={`/profile/${profileId}`}>
+                <button className='nav-link'><PersonIcon className='nav-profile' />Profile</button>
+              </Link>
+            </div>
+          </>
+        : ''
+    }
     </div>
   );
 }
